79bd8e635217503cced511c5a7558900826d851f
j_einspigl <j_einspigl@utb.cz>
2025-04-11 21:37:56 +0200

    Naming fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tabs/scan',
+    redirectTo: 'tabs/settings',
     pathMatch: 'full'
   },
   {
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: 'scan',
+        redirectTo: 'settings',
         pathMatch: 'full'
       }
     ]
