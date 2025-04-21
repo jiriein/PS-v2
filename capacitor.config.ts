7e68212f732b621125f12c2ac7f4b41635414ce4
j_einspigl <j_einspigl@utb.cz>
2025-04-21 18:36:18 +0200

    icons and splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'PS',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: '#15679c',
+      splashFullScreen: true,
+      splashImmersive: false
+    }
+  }
 };
 
 export default config;
